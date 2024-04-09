ffd2f7c1b516b872ec37d6b4ba875fde23f1cefd
Ville Salmela <villesalmela@users.noreply.github.com>
2024-04-09 22:57:05 +0300

    configure invoke tasks

---
+++ tasks.py
@@ -0,0 +1,17 @@
+from invoke.tasks import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/main.py")
+
+@task
+def test(ctx):
+    ctx.run("pytest src/tests", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src/tests", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
