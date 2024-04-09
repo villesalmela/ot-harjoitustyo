de7b45cd7d0e0867ce3673841c501b1880cc9c8b
Ville Salmela <villesalmela@users.noreply.github.com>
2024-04-09 22:54:45 +0300

    add packet class

---
+++ src/packet.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+class Packet:
+
+    def __init__(self, time: datetime, size: int) -> None:
+        self.time = time
+        self.size = size
+        self.layers = {}
+
+    def __str__(self) -> str:
+        out = f"time = {self.time}\n" + f"size = {self.size}\n\n"
+        for layer_type, layer in self.layers.items():
+            out += f"{layer_type.name}\n"
+            out += f"{layer}\n"
+
+        return f"### PACKET START ###\n{out.strip()}\n### PACKET END ###\n"
+
+    
