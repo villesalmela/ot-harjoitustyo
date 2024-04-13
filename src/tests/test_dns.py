f2bcdde47959fa8883997742015165a3cb4ca708
Ville Salmela <villesalmela@users.noreply.github.com>
2024-04-14 01:48:07 +0300

    adjust dns tests

--- src/tests/test_dns.py
+++ src/tests/test_dns.py
@@ -24,7 +24,7 @@ class TestDNS(unittest.TestCase):
         self.assertEqual(dns_layer.data["opcode"], DNSOpCode.QUERY)
         self.assertEqual(dns_layer.data["qtype"], DNSQType.TXT)
         self.assertEqual(dns_layer.data["name"], "google.com.")
-        self.assertIsNone(dns_layer.data["answer"])
+        self.assertIsNone(dns_layer.data["answers"])
 
     def test_dns_layer_response_data(self) -> None:
         dns_layer = self.parsed_packets[1].layers[LayerType.APPLICATION]
@@ -32,14 +32,14 @@ class TestDNS(unittest.TestCase):
         self.assertEqual(dns_layer.data["opcode"], DNSOpCode.QUERY)
         self.assertEqual(dns_layer.data["qtype"], DNSQType.TXT)
         self.assertEqual(dns_layer.data["name"], "google.com.")
-        self.assertEqual(dns_layer.data["answer"],
+        self.assertEqual(dns_layer.data["answers"],
                          [{'rrname': 'google.com.',
                            'type': 16,
                            'rclass': 1,
                            'ttl': 270,
                            'rdlen': 16,
-                           'rdata': [b'v=spf1 ptr ?all']}])
-        
+                           'rdata': ['v=spf1 ptr ?all']}])
+
     def test_2ld_extraction_normal(self) -> None:
         domain = extract_2ld("www.google.com")
         self.assertEqual(domain, "google.com")
@@ -47,7 +47,7 @@ class TestDNS(unittest.TestCase):
     def test_2ld_extraction_no_effect(self) -> None:
         domain = extract_2ld("google.com")
         self.assertEqual(domain, "google.com")
-    
+
     def test_2ld_extraction_fallback_single(self) -> None:
         domain = extract_2ld("localhost")
         self.assertEqual(domain, "localhost")
@@ -64,39 +64,6 @@ class TestDNS(unittest.TestCase):
         domain = extract_2ld("google.invalid")
         self.assertEqual(domain, "google.invalid")
 
-    def test_packet_str(self) -> None:
-        self.maxDiff = None
-        packet_str = str(self.parsed_packets[0])
-        expected = """### PACKET START ###
-time = 2005-03-30 11:47:46.496046
-size = 70
-
-LINK
-name = Ethernet
-size_total = 70
-size_payload = 56
-data = {'src_addr': '00:e0:18:b1:0c:ad', 'dst_addr': '00:c0:9f:32:41:8c'}
-
-NETWORK
-name = IP
-size_total = 56
-size_payload = 36
-data = {'src_addr': '192.168.170.8', 'dst_addr': '192.168.170.20'}
-
-TRANSPORT
-name = UDP
-size_total = 36
-size_payload = 28
-data = {'src_port': 32795, 'dst_port': 53}
-
-APPLICATION
-name = DNS
-size_total = 28
-size_payload = 0
-data = {'direction': <DNSDir.QUERY: 0>, 'opcode': <DNSOpCode.QUERY: 0>, 'qtype': <DNSQType.TXT: 16>, 'name': 'google.com.', 'answer': None}
-### PACKET END ###\n"""
-        self.assertEqual(packet_str, expected)
-
     def test_packet(self) -> None:
         packet = self.parsed_packets[0]
         self.assertEqual(packet.size, 70)
@@ -112,4 +79,3 @@ data = {'direction': <DNSDir.QUERY: 0>, 'opcode': <DNSOpCode.QUERY: 0>, 'qtype':
         self.assertEqual(layer.name, "DNS")
         self.assertTrue(layer == layer)
         self.assertFalse(layer == 1)
-
