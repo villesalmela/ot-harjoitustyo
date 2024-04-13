db077eaa73e092796df21da8b81ccc09b42344c7
Ville Salmela <villesalmela@users.noreply.github.com>
2024-04-14 01:42:33 +0300

    rename variable

--- src/layers/dns.py
+++ src/layers/dns.py
@@ -3,13 +3,14 @@ from layer_type import LayerType
 
 
 class DNS(LayerConfig):
-    def __init__(self, direction: int, opcode: int, qtype: int, name: str,
-                 answer: list[dict[str, str | int]] | None = None) -> None:
+    def __init__(self, transaction_id: int, direction: int, opcode: int, qtype: int, name: str,
+                 answers: list[dict[str, str | int]] | None = None) -> None:
 
         super().__init__(LayerType.APPLICATION, "DNS", {
+            "transaction_id": transaction_id,
             "direction": direction,
             "opcode": opcode,
             "qtype": qtype,
             "name": name,
-            "answer": answer
+            "answers": answers
         })
