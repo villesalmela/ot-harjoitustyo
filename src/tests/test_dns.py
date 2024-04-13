27a8a3e5e0b3af63d8fe28a96979ed4f8385a0ca
Ville Salmela <villesalmela@users.noreply.github.com>
2024-04-13 20:29:45 +0300

    formatting

--- src/tests/test_dns.py
+++ src/tests/test_dns.py
@@ -7,6 +7,7 @@ from dns_dir import DNSDir
 from dns_opcode import DNSOpCode
 from dns_qtype import DNSQType
 
+
 class TestDNS(unittest.TestCase):
     def setUp(self) -> None:
         self.parsed_packets = parse_pcap("assets/dns.pcap")
@@ -32,5 +33,10 @@ class TestDNS(unittest.TestCase):
         self.assertEqual(dns_layer.data["opcode"], DNSOpCode.QUERY)
         self.assertEqual(dns_layer.data["qtype"], DNSQType.TXT)
         self.assertEqual(dns_layer.data["name"], "google.com.")
-        self.assertEqual(dns_layer.data["answer"], [{'rrname': 'google.com.', 'type': 16, 'rclass': 1, 'ttl': 270, 'rdlen': 16, 'rdata': [b'v=spf1 ptr ?all']}])
-        
+        self.assertEqual(dns_layer.data["answer"],
+                         [{'rrname': 'google.com.',
+                           'type': 16,
+                           'rclass': 1,
+                           'ttl': 270,
+                           'rdlen': 16,
+                           'rdata': [b'v=spf1 ptr ?all']}])
