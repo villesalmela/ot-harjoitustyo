5bbc762d21708ceccdf66ec24e4d0efa418419c9
Ville Salmela <villesalmela@users.noreply.github.com>
2024-04-14 03:47:03 +0300

    improve type hinting

--- src/layers/dns.py
+++ src/layers/dns.py
@@ -1,10 +1,19 @@
 from layer_config import LayerConfig
 from layer_type import LayerType
+from dns_dir import DNSDir
+from dns_opcode import DNSOpCode
+from dns_qtype import DNSQType
 
 
 class DNS(LayerConfig):
-    def __init__(self, transaction_id: int, direction: int, opcode: int, qtype: int, name: str,
-                 answers: list[dict[str, str | int]] | None = None) -> None:
+    def __init__(self,
+                 transaction_id: int,
+                 direction: DNSDir,
+                 opcode: DNSOpCode,
+                 qtype: DNSQType,
+                 name: str,
+                 answers: list[dict[str, str | int]] | None = None
+                 ) -> None:
 
         super().__init__(LayerType.APPLICATION, "DNS", {
             "transaction_id": transaction_id,
