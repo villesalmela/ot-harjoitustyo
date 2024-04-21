b3890791c3c6b7adfb8e8b6eea2a1c38d4cd6326
Ville Salmela <villesalmela@users.noreply.github.com>
2024-04-21 14:54:07 +0300

    improve dns analyzer

--- src/analyzer/dns_analyzer.py
+++ src/analyzer/dns_analyzer.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 from components.packet import Packet as myPacket
 from layers.layer_level import LayerLevel
 from layers.properties.dns_dir import DNSDir
@@ -12,8 +10,11 @@ class DNSAnalyzer:
     def __init__(self, packets: list[myPacket]) -> None:
 
         # filter packets with DNS layer
-        self.packets = [packet for packet in packets if packet.layers[LayerLevel.APPLICATION] and
+        self.packets = [packet for packet in packets if LayerLevel.APPLICATION in packet.layers and
                         packet.layers[LayerLevel.APPLICATION].layer_name == "DNS"]
+
+        if not self.packets:
+            return
 
         # create dataframes
         self.dns_df = pd.DataFrame([flatten_dict(packet.layers[LayerLevel.APPLICATION].__dict__)
@@ -31,18 +32,18 @@ class DNSAnalyzer:
 
         self.enrich()
 
-        pd.set_option('display.max_columns', None)
-        pd.set_option('display.width', 300)
-        print(self.dns_df)
-
     def enrich(self) -> None:
         # parse 2LD from qname
         self.dns_df["data.2LD"] = self.dns_df["data.qname"].apply(extract_2ld)
 
     def most_queried_domains(self, n=10) -> dict[str, int]:
+        if not self.packets:
+            return {}
         return self.dns_df["data.2LD"].value_counts().head(n).to_dict()
 
     def most_common_servers(self, n=10) -> dict[str, int]:
+        if not self.packets or "data.dst_addr" not in self.dns_df.columns:
+            return {}
         # consider only data.direction = DNSDir.QUERY
         return self.dns_df[self.dns_df["data.direction"] ==
                            DNSDir.QUERY]["data.dst_addr"].value_counts().head(n).to_dict()
