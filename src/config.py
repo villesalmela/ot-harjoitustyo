b48a8e09bd0eca98efc43f6713591ba12e74e8a3
Ville Salmela <villesalmela@users.noreply.github.com>
2024-05-12 02:43:56 +0300

    configure logging

--- src/config.py
+++ src/config.py
@@ -1,6 +1,45 @@
 import os
+import sys
+import logging
+
 from dotenv import load_dotenv
 
-load_dotenv()  # handles searching for .env file and gracefully handles if it doesn't exist
 
+class StreamToLogger:
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+
+    Generated with ChatGPT.
+    """
+
+    def __init__(self, logger_name, log_level):
+        self.logger = logging.getLogger(logger_name)
+        self.log_level = log_level
+
+    def write(self, buffer):
+        for line in buffer.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+    def flush(self):
+        pass
+
+def configure_logging():
+    """
+    Redirects logs to a file.
+
+    stdout, stderr and all logs from the logging framework are redirected to a file.
+    """
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
+        filename="logs/app.log",
+        filemode="w"
+    )
+
+    sys.stdout = StreamToLogger("STDOUT", logging.INFO)
+    sys.stderr = StreamToLogger("STDERR", logging.ERROR)
+
+
+configure_logging()
+load_dotenv()  # handles searching for .env file and gracefully handles if it doesn't exist
 DB_PATH = os.getenv("DB_PATH", "database.db")
