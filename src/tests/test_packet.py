dc75a7945d0c8c86161b8a15a2801c1146a5c310
Ville Salmela <villesalmela@users.noreply.github.com>
2024-05-12 22:12:29 +0300

    remove unnecessary tests

--- src/tests/test_packet.py
+++ src/tests/test_packet.py
@@ -19,13 +19,9 @@ class TestPacket(unittest.TestCase):
         packet = self.parsed_packets[0]
         self.assertEqual(packet.size, 70)
         self.assertEqual(packet.time, datetime(2023, 2, 6, 20, 16, 7))
-        self.assertTrue(packet == packet)
-        self.assertFalse(packet == 1)
 
     def test_layer(self) -> None:
         layer = self.parsed_packets[0].layers[LayerLevel.APPLICATION]
         self.assertEqual(layer.size_total, 28)
         self.assertEqual(layer.size_payload, 0)
         self.assertEqual(layer.layer_name, "DNS")
-        self.assertTrue(layer == layer)
-        self.assertFalse(layer == 1)
